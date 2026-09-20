8d7c4ee10fca8053b902e41ea24b2e7f1bf1c6c2
honeyimstarving <chris@mercuryng.com>
2026-09-19 22:06:15 -0400

    Update revrise.js

--- api/revrise.js
+++ api/revrise.js
@@ -19,6 +19,7 @@
 // ─────────────────────────────────────────────────────────────
 
 const express = require('express');
+const fetch = require('node-fetch');
 const router = express.Router();
 
 const CTM_HOST   = 'api.calltrackingmetrics.com';
